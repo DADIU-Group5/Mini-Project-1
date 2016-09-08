e43e6d7ed74a4cb0c5a203a75a3ca43a34448d05
Nicholas Indrehus <nicholas.indrehus@gmail.com>
2016-09-08 15:07:50 +0200

    no message

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,13 +7,14 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
+    
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
