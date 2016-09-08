52b70057c716e889d8d8ca60326b7b95cc1fed3d
HLMN\DADIU <dadiu@DESK0460>
2016-09-08 14:01:43 +0200

    Initial code for moving using swipes.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,62 +4,109 @@ using System.Collections;
 /// <summary>
 /// Handles player movement + input.
 /// </summary>
-namespace Blub
+public class PlayerMovement : MonoBehaviour
 {
-    public class PlayerMovement : MonoBehaviour
+    private Vector3 leftLane = new Vector3(-3f, 0.5f, -8f);
+    private Vector3 middleLane = new Vector3(0f, 0.5f, -8f);
+    private Vector3 rightLane = new Vector3(3f, 0.5f, -8f);
+
+    private Vector2 touchOrigin = -Vector2.one;
+
+    // Current lane (0 - left, 1 - middle, 2 - right)
+    private int lane;
+
+    // Use this for initialization
+    void Start()
     {
-        private Vector3 leftLane = new Vector3(-3f, 0.5f, -8f);
-        private Vector3 middleLane = new Vector3(0f, 0.5f, -8f);
-        private Vector3 rightLane = new Vector3(3f, 0.5f, -8f);
+        lane = 1;
+    }
 
-        // Current lane (0 - left, 1 - middle, 2 - right)
-        private int lane;
+    // Update is called once per frame
+    void Update()
+    {
+        int horizontal = 0;     //Used to store the horizontal move direction.
+        int vertical = 0;       //Used to store the vertical move direction.
 
-        // Use this for initialization
-        void Start()
+            
+        //Check if Input has registered more than zero touches
+        if (Input.touchCount > 0)
         {
-            lane = 1;
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-
-            Transform transform = GetComponent<Transform>();
-
-            // Move left
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            //Store the first touch detected.
+            Touch myTouch = Input.touches[0];
+                
+            //Check if the phase of that touch equals Began
+            if (myTouch.phase == TouchPhase.Began)
             {
-                // Not in leftmost lane
-                if (lane != 0)
+                //If so, set touchOrigin to the position of that touch
+                touchOrigin = myTouch.position;
+            }
+                
+            //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                //Set touchEnd to equal the position of this touch
+                Vector2 touchEnd = myTouch.position;
+                    
+                //Calculate the difference between the beginning and end of the touch on the x axis.
+                float x = touchEnd.x - touchOrigin.x;
+                    
+                //Calculate the difference between the beginning and end of the touch on the y axis.
+                float y = touchEnd.y - touchOrigin.y;
+                    
+                //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+                touchOrigin.x = -1;
+                    
+                //Check if the difference along the x axis is greater than the difference along the y axis.
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                    //If x is greater than zero, set horizontal to 1, otherwise set it to -1
+                    horizontal = x > 0 ? 1 : -1;
+                else
+                    //If y is greater than zero, set horizontal to 1, otherwise set it to -1
+                    vertical = y > 0 ? 1 : -1;
+            }
+        }
+            
+        //Check if we have a non-zero value for horizontal or vertical
+        if (horizontal != 0 || vertical != 0)
+        {
+            //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+            //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+            AttemptMove(horizontal, vertical);
+        }
+    }
+
+    void AttemptMove(int horizontal, int vertical)
+    {
+        Transform transform = GetComponent<Transform>();
+        // Attempt move to left
+        if (horizontal < 0)
+        {
+            // Not in leftmost lane
+            if (lane != 0)
+            {
+                if (lane == 1)
                 {
-                    if (lane == 1)
-                    {
-                        transform.position = leftLane;
-                    }
-                    else
-                    {
-                        transform.position = middleLane;
-                    }
+                    transform.position = leftLane;
+                }
+                else
+                {
+                    transform.position = middleLane;
                 }
             }
+        }
+        else if (horizontal > 0)
+        {
 
-            // Move right
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            // Not in rightmost lane
+            if (lane != 2)
             {
-                // Not in leftmost lane
-                if (lane != 2)
+                if (lane == 1)
                 {
-                    if (lane == 1)
-                    {
-                        transform.position = rightLane;
-                    }
-                    else
-                    {
-                        transform.position = middleLane;
-                    }
+                    transform.position = rightLane;
+                }
+                else
+                {
+                    transform.position = middleLane;
                 }
             }
         }
