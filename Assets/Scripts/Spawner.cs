3e1985d9da1a8f2be6ff92d153b375e0c0d8c421
Camilla <camilla.g.jakobsen@hotmail.com>
2016-09-09 10:55:10 +0200

    Spawner number getting and random distance find, missing instantiate

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,9 +7,20 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
+    [Range(0, 2)]
+    public int minDistance = 0;
+
+    [Range(2,10)]
+    public int maxDistance = 2;
+
+    private float spawnDistance = 0.0f;
+
     public void Spawn(Vector3 pos)
     {
-        NumberGenerator._instance.GetNumber();
+        int newNumber = NumberGenerator._instance.GetNumber();
+        int randomDistance = Random.Range(minDistance, (maxDistance + 1));
+        Vector3 tempPosition = new Vector3(0, randomDistance, 0);
+        pos += tempPosition;
+        //instantiate + set number
     }
-
 }
