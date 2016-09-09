d1ed91922f6757559ef72339a77a127e020db3ce
Bjørn <ciprianliis@gmail.com>
2016-09-09 15:18:17 +0200

    no message

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -9,20 +9,29 @@ public class GameState : MonoBehaviour
     public static GameState _instance;
 
     // editable fields used for balancing game
+    // speed
+    [Header("Speed")]
     [Range(0, 50)]
     public float initialSpeed = 10;
+    [Range(1, 20)]
+    public int numbersPerSpeedIncrease = 10;
     [Range(0, 1)]
     public float speedMultiplierIncrease = 0.1f;
+    // score
+    [Header("Score")]
     [Range(1, 1000)]
     public int scorePerNumber = 10;
+    [Range(1, 20)]
+    public int numbersPerScoreIncrease = 10;
     [Range(0, 1)]
     public float scoreMultiplierIncrease = 0.1f;
+    // other
+    [Header("Other")]
+    public bool unbeatable;
     [Range(1, 10)]
     public int initialLifes = 3;
     [Range(1, 5)]
     public float laneWidth = 1.5f;
-    // Determines the width between the incoming numbers.
-    // This is also used for determining how much the player and car moves.
     [Range(1f, 3f)]
     public int missedNumbersThreshold = 5;
 
@@ -41,6 +50,7 @@ public class GameState : MonoBehaviour
         if (_instance == null)
         {
             _instance = this;
+            Init();
         }
         else
         {
@@ -49,7 +59,7 @@ public class GameState : MonoBehaviour
         }
 	}
 
-    void Start()
+    private void Init()
     {
         currentNumberSpeed = initialSpeed;
         playerLives = initialLifes;
@@ -82,9 +92,13 @@ public class GameState : MonoBehaviour
         {
             lastNumber = newNum;
             numberStreak++;
+
             // update speed
-            currentNumberSpeed = initialSpeed + (lastNumber / 10) * speedMultiplierIncrease;
-            currentScoreMultiplier = 1 + (numberStreak / 10) * scoreMultiplierIncrease;
+            int speedLevel = (lastNumber / numbersPerSpeedIncrease);
+            currentNumberSpeed = initialSpeed + initialSpeed * (speedLevel * speedMultiplierIncrease);
+
+            //update score
+            currentScoreMultiplier = 1 + (numberStreak / numbersPerScoreIncrease) * scoreMultiplierIncrease;
             score += scorePerNumber * currentScoreMultiplier;
             UIController._instance.UpdateScore((int)score);
         }
@@ -102,7 +116,7 @@ public class GameState : MonoBehaviour
         playerLives--;
         UIController._instance.UpdateLives(playerLives);
         // TODO Move cart away as player lose lives.
-        if (playerLives <= 0)
+        if (playerLives <= 0 && !unbeatable)
         {
             // game over
 
