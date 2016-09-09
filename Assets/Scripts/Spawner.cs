26f05634d7ae2aff4b51df2474bff3f07b1603c9
Mia Louise Søgaard <ciprianliis@gmail.com>
2016-09-09 10:44:00 +0200

    Added a 'Number' tag. Adding 'player catches number' logic to player script. Going to work on gamestate now.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,11 +7,21 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
+    public GameObject numberPrefab;
+
     private NumberGenerator numberGenerator;
+
+    void Start()
+    {
+        Debug.Log("Spawning");
+        Spawn(gameObject.transform.position);
+    }
 
     public void Spawn(Vector3 pos)
     {
         numberGenerator.GetNumber();
+        Debug.Log("instantiating");
+        GameObject number = (GameObject)Instantiate(numberPrefab) as GameObject;
     }
 
 }
