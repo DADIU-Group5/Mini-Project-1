bf9a7f2166f334c028c53ddc404a81525b8962fd
Mia Louise Søgaard <ciprianliis@gmail.com>
2016-09-12 12:22:39 +0200

    Added vegetation spawning and movement along the ground.

---
+++ Assets/Scripts/Despawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Despawner : MonoBehaviour {
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = gameObject.transform.position;
+    }
+
+    //spawn rate based on size
+    //lasttimespawned
+
+    void Update()
+    {
+        if (gameObject.transform.position.z <= -13f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/VegetationSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VegetationSpawner : MonoBehaviour {
+
+    GameObject wheel;
+    public GameObject[] bushes;
+    public float spawnTime = 0.5f;
+    
+	// Use this for initialization
+	void Start () {
+        wheel = GameObject.Find("round");
+        Vegetation();
+    }
+
+    public void SpawnObject()
+    {
+        GameObject vegetation = bushes[Random.Range(0, bushes.Length)];
+
+        float xCoord = Random.Range(7, 45);
+        if (Random.Range(0,2) == 1)
+        {
+            xCoord = -xCoord;
+        }
+        // spawn
+        vegetation = (GameObject)Instantiate(vegetation, transform.position + new Vector3(xCoord, -21.74f, 93.37f) + (transform.right), Quaternion.Euler(24.3f,0,0 ));
+
+        // make as child
+        vegetation.transform.SetParent(wheel.gameObject.transform);
+    }
+
+    public void Vegetation()
+    {
+        InvokeRepeating("SpawnObject", 0f, spawnTime);
+    }
+}
