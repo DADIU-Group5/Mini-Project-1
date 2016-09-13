5f1861c75f9c050658b0155defce05d2786df785
Bjørn Vinther <bvinther@hotmail.com>
2016-09-13 13:43:59 +0200

    simpel swipe

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,12 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     enum Lane { Left, Middle, Right };
+
+    // editable fields
+    [Range(0, 10000)]
+    public int pixelForSwipe = 400;
+
+
     private Vector3 leftLane = new Vector3(-2f, 0f, -14.5f);
     private Vector3 middleLane = new Vector3(0f, 0f, -14.5f);
     private Vector3 rightLane = new Vector3(2f, 0f, -14.5f);
@@ -14,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 touchOrigin = -Vector2.one;
 
     private Animator hugoAnimator;
+    private bool canSwipe;
 
     // Current lane (0 - left, 1 - middle, 2 - right)
     private Lane lane;
@@ -36,6 +43,7 @@ public class PlayerMovement : MonoBehaviour
         leftLane = new Vector3(0f - GameState._instance.laneWidth, 0f, -14.5f);
         rightLane = new Vector3(0f + GameState._instance.laneWidth, 0f, -14.5f);
         hugoAnimator = GetComponentInChildren<Animator>();
+        canSwipe = true;
     }
 
     // Update is called once per frame
@@ -50,11 +58,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            AttemptMove(-1, 0);
+            AttemptMove(-1);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            AttemptMove(1, 0);
+            AttemptMove(1);
         }
 
         #endif
@@ -71,42 +79,27 @@ public class PlayerMovement : MonoBehaviour
             {
                 //If so, set touchOrigin to the position of that touch
                 touchOrigin = myTouch.position;
+                canSwipe = true;
             }
                 
             //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
-            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            else if (myTouch.phase == TouchPhase.Moved && canSwipe)
             {
-                //Set touchEnd to equal the position of this touch
-                Vector2 touchEnd = myTouch.position;
-                    
-                //Calculate the difference between the beginning and end of the touch on the x axis.
-                float x = touchEnd.x - touchOrigin.x;
-                    
-                //Calculate the difference between the beginning and end of the touch on the y axis.
-                float y = touchEnd.y - touchOrigin.y;
-                    
-                //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
-                touchOrigin.x = -1;
-                    
-                //Check if the difference along the x axis is greater than the difference along the y axis.
-                if (Mathf.Abs(x) > Mathf.Abs(y))
-                    //If x is greater than zero, set horizontal to 1, otherwise set it to -1
-                    horizontal = x > 0 ? 1 : -1;
-                else
-                    //If y is greater than zero, set horizontal to 1, otherwise set it to -1
-                    vertical = y > 0 ? 1 : -1;
+                // right
+                if (myTouch.position.x - touchOrigin.x > pixelForSwipe) {
+                    AttemptMove(1);
+                    canSwipe = false;
+                }
+
+                //left
+                if (myTouch.position.x - touchOrigin.x < -pixelForSwipe)
+                {
+                    AttemptMove(-1);
+                    canSwipe = false;
+                }
             }
         }
-            
-        //Check if we have a non-zero value for horizontal or vertical
-        if (horizontal != 0 || vertical != 0)
-        {
-            //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
-            //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
-            AttemptMove(horizontal, vertical);
-        }
-
-
+        
         // Check if we are doing the jumping animation.
         if (Time.timeSinceLevelLoad < jumpEndTime)
         {
@@ -126,7 +119,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void AttemptMove(int horizontal, int vertical)
+    void AttemptMove(int horizontal)
     {
         if (!canMove)
         {
