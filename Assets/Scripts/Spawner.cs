5bc43ec7baa59a31f673ba3bc9801c5d7a49c3fa
DADIU <mabiscus@gmail.com>
2016-09-13 15:32:22 +0200

    Changed time references to gamestate time-management in number and spawner.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -38,7 +38,8 @@ public class Spawner : MonoBehaviour {
         //instantiate + set number
         Number number = Instantiate(numberPrefab);
         number.ThisNumber = newNumber;
-        number.throwEndTime = Time.timeSinceLevelLoad + throwTime;
+        //number.throwEndTime = Time.timeSinceLevelLoad + throwTime;
+        number.throwEndTime = GameState._instance.GetTimeSinceGameStarted() + throwTime;
         number.throwHeight = throwHeight;
         number.throwStartPos = new Vector3(pos.x, 2.5f, pos.z);
         number.throwEndPos = new Vector3(pos.x, 0.5f, spawnZ);
